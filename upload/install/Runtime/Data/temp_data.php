9065ded3a2652ec159b60b2d9360014e25f04496
ysc8620 <ysc8620@163.com>
2015-08-16 15:27:58 +0800

    ds

---
+++ upload/install/Runtime/Data/temp_data.php
@@ -0,0 +1,1 @@
+{"db_host":"localhost","db_port":"3306","db_user":"root","db_pass":"LEsc2008","db_name":"pinphp","db_prefix":"pin_","admin_user":"admin","admin_pass":"admin","admin_pass_confirm":"admin","admin_email":"admin@163.com"}
